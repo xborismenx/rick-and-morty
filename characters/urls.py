5e6719c7c1e749c92af35c5f8677c97df9835b3b
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-24 17:22:16 +0300

    added Character list endpoint with filter

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from characters.views import get_random_character
+from characters.views import get_random_character, CharacterListView
 
 urlpatterns = [
     path("characters/random/", get_random_character, name="character-random"),
+    path("characters/", CharacterListView.as_view(), name="character-list"),
 ]
 
 app_name = "characters"
--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,6 @@
 import random
 
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -9,10 +9,21 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_random_character(request: Request) -> Response:
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
