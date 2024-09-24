91534ae7b9e4e3e3a67b1042acae521afdcef2bc
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-24 19:04:52 +0300

    added swagger documentation using drf-spectacular

--- characters/views.py
+++ characters/views.py
@@ -1,5 +1,6 @@
 import random
 
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
@@ -9,8 +10,10 @@ from characters.models import Character
 from characters.serializers import CharacterSerializer
 
 
+@extend_schema(responses={status.HTTP_200_OK: CharacterSerializer()})
 @api_view(["GET"])
 def get_random_character(request: Request) -> Response:
+    """Get a random character from Rick & Morty"""
     pks = Character.objects.values_list("pk", flat=True)
     random_pk = random.choice(pks)
     random_character = Character.objects.get(pk=random_pk)
@@ -27,3 +30,17 @@ class CharacterListView(generics.ListAPIView):
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filter by name insensitive contains",
+                required=False,
+                type=str,
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        """List characters with filter by name"""
+        return super().list(request, *args, **kwargs)
--- rick_and_morty_api/urls.py
+++ rick_and_morty_api/urls.py
@@ -14,10 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/", include("characters.urls", namespace="characters")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
