e26864987f97a894978cd68d625d4b14dbc592d4
xborismenx <boris.tikhonov.21@gmail.com>
2024-09-26 20:10:56 +0300

    fixed save_characters to handle integrity error

--- characters/scraper.py
+++ characters/scraper.py
@@ -1,4 +1,5 @@
 import requests
+from django.db import IntegrityError
 
 from characters.models import Character
 from django.conf import settings
@@ -27,7 +28,10 @@ def scrape_characters() -> list[Character]:
 
 def save_characters(characters: list[Character]) -> None:
     for character in characters:
-        character.save()
+        try:
+            character.save()
+        except IntegrityError:
+            print(f"character with api_id {character.api_id} already exists")
 
 
 def sync_characters_with_api() -> None:
