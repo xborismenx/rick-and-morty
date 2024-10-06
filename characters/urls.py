f35671eeac83ac50836d2318c089021691ffff6d
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-06 17:28:39 +0300

    changed name get_random_character on get_random_character_view & added docker-compose settings

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from characters.views import get_random_character, CharacterListView
+from characters.views import get_random_character_view, CharacterListView
 
 urlpatterns = [
-    path("characters/random/", get_random_character, name="character-random"),
+    path("characters/random/", get_random_character_view, name="character-random"),
     path("characters/", CharacterListView.as_view(), name="character-list"),
 ]
 
